4d0e3e071c6e302e162fe3ffb93089debb79e543
Morgane Goibert <morgane.goibert@gmail.com>
2019-05-28 14:46:07 +0200

    ENH: comparison clean vs adversarial distance distrib

--- peristent_diagram_NN.py
+++ peristent_diagram_NN.py
@@ -9,16 +9,13 @@ Author: Morgane Goibert <morgane.goibert@gmail.com>
 from IPython.display import Image
 from os import chdir
 import numpy as np
-import gudhi as gd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import dionysus as d
 from scipy.spatial.distance import squareform
 
-import numpy as np
 import scipy.stats as stats
 import scipy.sparse as sparse
-import matplotlib.pyplot as plt
 from numpy import inf
 from scipy.linalg import block_diag
 
@@ -139,7 +136,7 @@ test_loader.dataset = tuple(zip(map(lambda x: x.double(), map(itemgetter(0),
 
 # Train the NN
 
-num_epochs = 15
+num_epochs = 30
 optimizer = optim.SGD(model.parameters(), lr=0.1)
 loss_history = []
 loss_func = nn.CrossEntropyLoss()
@@ -175,6 +172,7 @@ with torch.no_grad():
         pred = output.argmax(dim=1, keepdim=True)
         correct += pred.eq(target.view_as(pred)).sum().item()
 acc = correct / len(val_loader.dataset)
+print("Val accuracy =", acc)
 
 # Compute test acc
 model.eval()
@@ -185,15 +183,16 @@ with torch.no_grad():
         output = model(data)
         pred = output.argmax(dim=1, keepdim=True)
         correct += pred.eq(target.view_as(pred)).sum().item()
-
 acc = correct / len(test_loader.dataset)
+print("Test accuracy =", acc)
 
 
 # Paramaters
 numero_ex = 5
 adversarial = True
 epsilon = 0.25
-def compute_persistent_dgm(model, test_set, numero_ex=0, adversarial=False, epsilon= .25):
+
+def compute_persistent_dgm(model, test_set, numero_ex=0, adversarial=False, epsilon= .25, threshold=0):
 
     t0 = time.time()
     # Get the parameters of the model
@@ -247,26 +246,34 @@ def compute_persistent_dgm(model, test_set, numero_ex=0, adversarial=False, epsi
 
     # Create the final weight matrix using blocks
     val0, val2, val4= val0.detach().numpy(), val2.detach().numpy(), val4.detach().numpy()
-    val0, val2, val4 = 1000*np.abs(val0), 1000*np.abs(val2), 1000*np.abs(val4)
-    val0, val2, val4 = np.around(val0, decimals=7), np.around(val2, decimals=7), np.around(val4, decimals=7)
+    val0, val2, val4 = 10e5*np.abs(val0), 10e5*np.abs(val2), 10e5*np.abs(val4)
+    val0, val2, val4 = np.around(val0, decimals=2), np.around(val2, decimals=2), np.around(val4, decimals=2)
     
     # Fast implementation but "by hand"
     vec = []
-
+    
+    # Adding the edges
+    # If the edge value is too small (< threshold), then there is no edge.
+    # 0 is not a possible value for the model parameter, but a very small value
+    # can be considered as a 0.
     for row in range(val0.shape[0]):
         for col in range(val0.shape[1]):
-            vec.append( ([row+val0.shape[1], col], val0[row,col]) )
+            if val0[row,col] >= threshold:
+                vec.append( ([row+val0.shape[1], col], val0[row,col]) )
         
     for row in range(val2.shape[0]):
         for col in range(val2.shape[1]):
-            vec.append( ([row+val0.shape[1]+val2.shape[1], col+val0.shape[1]], val2[row,col]) )
+            if val2[row,col] >= threshold:
+                vec.append( ([row+val0.shape[1]+val2.shape[1], col+val0.shape[1]], val2[row,col]) )
 
     for row in range(val4.shape[0]):
         for col in range(val4.shape[1]):
-            vec.append( ([row+val0.shape[1]+val2.shape[1]+val4.shape[1], 
+            if val4[row,col] >= threshold:
+                vec.append( ([row+val0.shape[1]+val2.shape[1]+val4.shape[1], 
                       col+val0.shape[1]+val2.shape[1]], val4[row,col]) )
     
     # Fast implementation
+    # Adding the vertices
     nb_vertices = max([elem for array in tuple(map(itemgetter(0), vec)) for elem in array])
 
     dict_vertices = {key: [] for key in range(nb_vertices+1)}
@@ -274,7 +281,9 @@ def compute_persistent_dgm(model, test_set, numero_ex=0, adversarial=False, epsi
         dict_vertices[edge[0]].append(timing)
         dict_vertices[edge[1]].append(timing)
     for vertex in dict_vertices:
-        vec.append( ([vertex], min(dict_vertices[vertex])) )
+        if len(dict_vertices[vertex]) > 0:
+            vec.append( ([vertex], min(dict_vertices[vertex])) )
+    
     
     f = d.Filtration()
     for vertices, timing in vec:
@@ -331,67 +340,74 @@ def get_class_indices(label_wanted, number=2, start_i=0, test_set=test_set):
     return valid_numero
         
 #Get the indices for the class we want
-inds_clean = get_class_indices(6)
+inds_clean = get_class_indices(1, number=10)
 
 # Distances between a clean and an adversarial input
-dgms_clean = compute_persistent_dgm(model, test_set, numero_ex=inds_clean[0], adversarial=False, epsilon= .25)
-dgms_adv = compute_persistent_dgm(model, test_set, numero_ex=inds_clean[0], adversarial=True, epsilon= .25)
+dgms_clean = compute_persistent_dgm(model, test_set, numero_ex=inds_clean[9], adversarial=False, epsilon= .25, threshold=5000)
+dgms_adv = compute_persistent_dgm(model, test_set, numero_ex=inds_clean[9], adversarial=True, epsilon= .25, threshold=5000)
 wdist = d.wasserstein_distance(dgms_clean[0][0], dgms_adv[0][0], q=2)
 
 # Distances between two clean inputs
-dgms_clean2 = compute_persistent_dgm(model, test_set, numero_ex=inds_clean[1], adversarial=False, epsilon= .25)
+dgms_clean2 = compute_persistent_dgm(model, test_set, numero_ex=inds_clean[5], adversarial=False, epsilon= .25, threshold=5000)
 wdist_clean = d.wasserstein_distance(dgms_clean[0][0], dgms_clean2[0][0], q=2)
 
 # Get indices for the adv predicted class
-inds_adv = get_class_indices(dgms_adv[3])
+inds_adv = get_class_indices(dgms_adv[3], number=10)
 
 # Distances between the adv exemple and the target class
-dgms_compar_adv = compute_persistent_dgm(model, test_set, numero_ex=inds_adv[0], adversarial=False, epsilon= .25)
+dgms_compar_adv = compute_persistent_dgm(model, test_set, numero_ex=inds_adv[9], adversarial=False, epsilon= .25, threshold=5000)
 wdist_compar_adv = d.wasserstein_distance(dgms_adv[0][0], dgms_compar_adv[0][0], q=2)
 
-print("Distance clean/adv = %s, distance clean = %s, distance adv/wrong class = %s" 
+# Distance between the clean input and the target class input
+wdist_clean_target = d.wasserstein_distance(dgms_clean[0][0], dgms_compar_adv[0][0], q=2)
+
+print("Distance clean/adv = %s, distance clean = %s, distance adv/wrong class = %s, and distance clean/wrong = %s" 
       %(np.around(wdist, decimals=2), np.around(wdist_clean, decimals=2),
-        np.around(wdist_compar_adv, decimals=2)))
+        np.around(wdist_compar_adv, decimals=2), np.around(wdist_clean_target, decimals=2)))
 
 
 
 # ------
-# Compute intra-class distribution of distances
+# Compute distributions of distances
 # ------
 
 # We want to display the distance distribution of a class, and the distance
 # distribution of an av. input from that class (but misclassified into another
 # class by the model) to the data from the original class.
-inds_class = get_class_indices(0, number="all")
-inds_class = inds_class[:100]
-dgms_dict = {key: [] for key in inds_class}
 
-for index in inds_class:
-    dgms_dict[index].append( compute_persistent_dgm(model, test_set, numero_ex=index)[0] )
+n=10
+
+inds_class = get_class_indices(0, number="all")
+inds_class2 = inds_class[:n]
+dgms_dict = {key: [] for key in inds_class2}
+
+for index in inds_class2:
+    dgms_dict[index].append( compute_persistent_dgm(model, test_set, numero_ex=index, threshold=5000)[0] )
 
 dist_vec = []
-for i, ind1 in enumerate(inds_class):
+for i, ind1 in enumerate(inds_class2):
     print(i)
-    for j in range(i+1, len(inds_class)):
-        dist_vec.append( d.wasserstein_distance(dgms_dict[ind1][0][0], dgms_dict[inds_class[j]][0][0], q=2) )
+    for j in range(i+1, len(inds_class2)):
+        ind2 = inds_class2[j]
+        dist_vec.append( d.wasserstein_distance(dgms_dict[ind1][0][0], dgms_dict[ind2][0][0], q=2) )
 
 sns.distplot(dist_vec)
 
 
 # Compute the distance between an adversarial and the class
-dgms_adv_dist = compute_persistent_dgm(model, test_set, numero_ex=inds_class[101], adversarial=True, epsilon= .25)
-wrong_class = dgms_adv_dist[3]
+dgms_adv = compute_persistent_dgm(model, test_set, numero_ex=inds_class[n+1], adversarial=True, epsilon= .25, threshold=5000)
+wrong_class = dgms_adv[3]
 
 dist_vec_adv = []
-for i, ind1 in enumerate(inds_class):
+for i, ind1 in enumerate(inds_class2):
     print(i)
-    dist_vec_adv.append( d.wasserstein_distance(dgms_dict[ind1][0][0], dgms_adv_dist[0][0], q=2) )
+    dist_vec_adv.append( d.wasserstein_distance(dgms_dict[ind1][0][0], dgms_adv[0][0], q=2) )
 
 sns.distplot(dist_vec_adv)
 
 # On the same plot
-sns.distplot(dist_vec, hist=False)
-sns.distplot(dist_vec_adv, hist=False)
+sns.distplot(dist_vec, hist=False, label="clean")
+sns.distplot(dist_vec_adv, hist=False, label="clean/adv")
 sns.plt.show()
 
 
@@ -400,30 +416,31 @@ sns.plt.show()
 # wrong predicted class for the adv. input), and the distance distribution
 # of the adv. input and this "wrong" class (thus same prediction class)
 inds_class_wrong = get_class_indices(wrong_class, number="all")
-inds_class_wrong = inds_class_wrong[:100]
-dgms_dict_wrong = {key: [] for key in inds_class_wrong}
+inds_class_wrong2 = inds_class_wrong[:n]
+dgms_dict_wrong = {key: [] for key in inds_class_wrong2}
 
-for index_wrong in inds_class_wrong:
-    dgms_dict_wrong[index_wrong].append( compute_persistent_dgm(model, test_set, numero_ex=index_wrong)[0] )
+for index_wrong in inds_class_wrong2:
+    dgms_dict_wrong[index_wrong].append( compute_persistent_dgm(model, test_set, numero_ex=index_wrong, threshold=5000)[0] )
 
 dist_vec_wrong = []
-for i, ind1 in enumerate(inds_class_wrong):
+for i, ind1 in enumerate(inds_class_wrong2):
     print(i)
-    for j in range(i+1, len(inds_class_wrong)):
-        dist_vec_wrong.append( d.wasserstein_distance(dgms_dict_wrong[ind1][0][0], dgms_dict_wrong[inds_class_wrong[j]][0][0], q=2) )
+    for j in range(i+1, len(inds_class_wrong2)):
+        ind2 = inds_class_wrong2[j]
+        dist_vec_wrong.append( d.wasserstein_distance(dgms_dict_wrong[ind1][0][0], dgms_dict_wrong[ind2][0][0], q=2) )
 
 sns.distplot(dist_vec_wrong)
 
 dist_vec_adv_wrong = []
-for i, ind1 in enumerate(inds_class_wrong):
+for i, ind1 in enumerate(inds_class_wrong2):
     print(i)
-    dist_vec_adv_wrong.append( d.wasserstein_distance(dgms_dict_wrong[ind1][0][0], dgms_adv_dist[0][0], q=2) )
+    dist_vec_adv_wrong.append( d.wasserstein_distance(dgms_dict_wrong[ind1][0][0], dgms_adv[0][0], q=2) )
 
 sns.distplot(dist_vec_adv_wrong)
 
 # On the same plot
-sns.distplot(dist_vec_wrong, hist=False)
-sns.distplot(dist_vec_adv_wrong, hist=False)
+sns.distplot(dist_vec_wrong, hist=False, label="wrong")
+sns.distplot(dist_vec_adv_wrong, hist=False, label="wrong/adv")
 sns.plt.show()
 
 
@@ -432,24 +449,159 @@ sns.distplot(dist_vec, hist=False, label="clean")
 sns.distplot(dist_vec_adv, hist=False, label="clean/adv")
 sns.distplot(dist_vec_wrong, hist=False, label="wrong")
 sns.distplot(dist_vec_adv_wrong, hist=False, label="wrong/adv")
-sns.plt.show()
+sns.show()
 
 
 # We want to display the distance between the orginal class and the "wrong" class
 dist_vec_origin_wrong = []
-for i, ind1 in enumerate(inds_class[0:25]):
+for i, ind1 in enumerate(inds_class2):
     print(i)
-    for j, ind2 in enumerate(inds_class_wrong[0:25]):
+    for j, ind2 in enumerate(inds_class_wrong2):
         dist_vec_origin_wrong.append( d.wasserstein_distance(dgms_dict[ind1][0][0], dgms_dict_wrong[ind2][0][0], q=2) )
 
-sns.distplot(dist_vec_origin_wrong, hist=False, label="origin/wrong")
 sns.distplot(dist_vec, hist=False, label="clean")
-sns.distplot(dist_vec_adv, hist=False, label="clean/adv")
 sns.distplot(dist_vec_wrong, hist=False, label="wrong")
+sns.distplot(dist_vec_origin_wrong, hist=False, label="origin/wrong")
+sns.distplot(dist_vec_adv, hist=False, label="clean/adv")
 sns.distplot(dist_vec_adv_wrong, hist=False, label="wrong/adv")
 sns.plt.show()
 
 
+
+
+
+
+
+# ------
+# Compute all distribution of distances
+# ------
+
+n=50
+all_class = range(10)
+
+# n indices for every class
+inds_all_class = {key: get_class_indices(key, number="all")[:n] for key in all_class}
+
+# Dict containing n dgms for each class (key = class, "dgms_i"; value = dgms for
+# each index for each class)
+dgms_dict = {}
+for i in inds_all_class.keys():
+    dict_temp = {}
+    for index in inds_all_class[i]:
+        dict_temp[index] = compute_persistent_dgm(model, test_set, numero_ex=index, threshold=5000)
+    dgms_dict["dgms_" + str(i)] = dict_temp
+
+# Building the dictionary containing the distance distributions
+distrib_dist = {}
+
+# Step 1: intra-class distances distributions
+for k, key in enumerate(dgms_dict.keys()):
+    dist_temp = []
+    print("key =", key)
+    for i, ind1 in enumerate(dgms_dict[key].keys()):
+        print("ind1 =", ind1)
+        for j in range(i+1, len(dgms_dict[key].keys())):
+            ind2 = list(dgms_dict[key].keys())[j]
+            print("ind2 =", ind2)
+            dist_temp.append( d.wasserstein_distance(dgms_dict[key][ind1][0][0], dgms_dict[key][ind2][0][0], q=2) )
+    distrib_dist["dist_"+str(k)+"_"+str(k)] = dist_temp
+
+# Step 2: inter-class distribution
+for class1, _ in enumerate(all_class):
+    key1 = "dgms_" + str(class1)
+    print("key1 =", key1)
+    for class2 in range(class1+1, len(all_class)):
+        key2 = "dgms_" + str(class2)
+        print("key2 =", key2)
+        dist_temp = []
+        for i, ind1 in enumerate(dgms_dict[key1].keys()):
+            for j, ind2 in enumerate(dgms_dict[key2].keys()):
+                print("inds =", ind1, ind2)
+                dist_temp.append( d.wasserstein_distance(dgms_dict[key1][ind1][0][0], dgms_dict[key2][ind2][0][0], q=2) )
+        distrib_dist["dist_"+str(class1)+"_"+str(class2)] = dist_temp
+
+
+sns.set()
+# Plots
+for i in all_class:
+    sns.distplot(distrib_dist["dist_"+str(i)+"_"+str(i)], hist=False, label=str(i))
+
+for i in all_class:
+    sns.distplot(distrib_dist["dist_0_"+str(i)], hist=False, label="0 and "+str(i))
+
+
+sns.distplot(distrib_dist["dist_1_1"], hist=False, label="1")
+sns.distplot(distrib_dist["dist_7_7"], hist=False, label="7")
+sns.distplot(distrib_dist["dist_1_7"], hist=False, label="1 and 7")
+
+
+
+
+# ------
+# Compute persistent diagrams and distances for several adversarial examples
+# ------
+
+n=50
+all_class = range(10)
+
+# n indices for every class
+inds_all_class_adv = {key: get_class_indices(key, number="all")[n:2*n] for key in all_class}
+
+# Dict containing n adv. dgms indexed by the target "wrong" class the model
+# (badly) predicted
+dgms_dict_adv = {}
+dict_temp = {}
+for i in inds_all_class_adv.keys():
+    for index in inds_all_class_adv[i]:
+        dict_temp[index] = compute_persistent_dgm(model, test_set, numero_ex=index, threshold=5000, adversarial=True)
+
+for i in inds_all_class_adv.keys():
+    temp = {}
+    for index in dict_temp.keys():
+        if dict_temp[index][3] == i:
+            temp[index] = dict_temp[index]
+    dgms_dict_adv["dgms_" + str(i)] = temp
+        
+
+# Building the dictionary containing the distance distributions between adv
+# examples and the target "wrong" class.
+distrib_dist_adv = {}
+
+for k, key in enumerate(dgms_dict.keys()):
+    dist_temp = []
+    print("key =", key)
+    for i_adv, ind_adv in enumerate(dgms_dict_adv[key].keys()):
+        print("ind_adv =", ind_adv)
+        for j, ind_clean in enumerate(dgms_dict[key].keys()):
+            print("ind clean =", ind_clean)
+            dist_temp.append( d.wasserstein_distance(dgms_dict_adv[key][ind_adv][0][0], dgms_dict[key][ind_clean][0][0], q=2) )
+    distrib_dist_adv["dist_adv_"+str(k)] = dist_temp
+
+for i in all_class:
+    sns.distplot(distrib_dist_adv["dist_adv_"+str(i)], hist=False, label=str(i))
+
+
+
+# ------
+# Differences between the "clean" distance distrib and the adversarial one
+# ------
+
+for i in all_class:
+    sns.distplot(distrib_dist["dist_"+str(i)+"_"+str(i)], hist=False, label="clean "+str(i))
+    sns.distplot(distrib_dist_adv["dist_adv_"+str(i)], hist=False, label="adv "+str(i))
+    plt.show()
+
+
+
+# ------
+# Differences between the "clean" distance distrib and !!noise!! distrib
+# ------
+
+
+
+# ----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 
 # -----
@@ -492,3 +644,7 @@ d.plot.plot_diagram(dgms[0], show = True)
 
 # ------ Dev / brouillon
 
+a = {0: [1,2,3], 1: [10,20,30]}
+b = {}
+for i in a.keys():
+    b["dgms_" + str(i)] = "test"+str(i)
