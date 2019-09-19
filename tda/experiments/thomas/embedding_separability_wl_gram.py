a2c966964d5d8e41ed1a8a8a652e3c684688953a
Thomas Ricatte <thomas@ricatte.fr>
2019-09-19 11:47:11 +0200

    Adding Gram-based experiment

---
+++ tda/experiments/thomas/embedding_separability_wl_gram.py
@@ -0,0 +1,30 @@
+import json
+from r3d3 import R3D3Experiment
+from tda.rootpath import rootpath, db_path
+from tda.embeddings import EmbeddingType
+from tda.embeddings.weisfeiler_lehman import NodeLabels
+
+experiment = R3D3Experiment(
+    db_path=db_path,
+    configs={
+        'embedding_type': [
+            EmbeddingType.WeisfeilerLehman
+            ],
+        'threshold': [
+            20000
+        ],
+        'height': [
+            5
+        ],
+        'hash_size': [
+            50
+        ],
+        'node_labels': [
+            NodeLabels.NONE,
+            NodeLabels.LAYERS
+        ]
+
+    },
+    binary=f"{rootpath}/tda/experiments/thomas/embedding_separability_binary_gram.py",
+    max_nb_processes=1
+)
