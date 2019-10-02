77e4725b34ec48c10c1bf9cc4945550bf932f962
Thomas Ricatte <thomas@ricatte.fr>
2019-10-02 14:55:14 +0200

    Improving API of get_deep_model for other datasets

--- test/test_models.py
+++ test/test_models.py
@@ -1,8 +1,13 @@
 from tda.models import get_deep_model
 from tda.models.datasets import Dataset
+from tda.models.architectures import mnist_mlp
 
 
 def test_get_mnist_model():
     source_dataset = Dataset("MNIST")
-    get_deep_model(dataset=source_dataset, num_epochs=1)
+    get_deep_model(
+        dataset=source_dataset,
+        num_epochs=1,
+        architecture=mnist_mlp
+    )
 
