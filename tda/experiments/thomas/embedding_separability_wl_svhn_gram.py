26eff7ffeb6f3bea082ace4bb3e4871a2ae21a36
Thomas Ricatte <thomas@ricatte.fr>
2019-10-03 10:54:50 +0200

    Adding first xp with SVHN

---
+++ tda/experiments/thomas/embedding_separability_wl_svhn_gram.py
@@ -0,0 +1,39 @@
+import json
+from r3d3 import R3D3Experiment
+from tda.rootpath import rootpath, db_path
+from tda.embeddings import EmbeddingType
+from tda.embeddings.weisfeiler_lehman import NodeLabels
+from tda.models.architectures import svhn_cnn_simple
+
+experiment = R3D3Experiment(
+    db_path=db_path,
+    configs={
+        'embedding_type': [
+            EmbeddingType.WeisfeilerLehman
+            ],
+        'threshold': [
+            20000
+        ],
+        'height': [
+            5
+        ],
+        'hash_size': [
+            50
+        ],
+        'node_labels': [
+            NodeLabels.LAYERS
+        ],
+        'noise': [
+            0.02
+        ],
+        'architecture': [
+            svhn_cnn_simple.name
+        ],
+        'dataset': [
+            "SVHN"
+        ]
+
+    },
+    binary=f"{rootpath}/tda/experiments/thomas/embedding_separability_binary_gram.py",
+    max_nb_processes=1
+)
