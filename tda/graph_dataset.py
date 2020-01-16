c737b8575258a990b01af65d14d646d84616fcfc
Morgane Goibert <morgane.goibert@gmail.com>
2020-01-16 17:50:10 +0100

    ENH: saving datasets of examples + separating edge dict and threshold

--- tda/graph_dataset.py
+++ tda/graph_dataset.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import typing
+import os
 
 import numpy as np
 import torch
@@ -11,6 +12,7 @@ from tda.models.attacks import FGSM, BIM, DeepFool, CW
 from tda.models.datasets import Dataset
 from tda.devices import device
 from tda.logging import get_logger
+from tda.rootpath import rootpath
 
 logger = get_logger("GraphDataset")
 
@@ -107,6 +109,91 @@ def process_sample(
 
     return x, y
 
+def load_saved_ex_dataset(
+        epsilon: float,
+        noise: float,
+        adv: bool,
+        dataset: Dataset,
+        architecture: Architecture = mnist_mlp,
+        dataset_size: int = 100,
+        attack_type: str = "FGSM",
+        train: bool = True
+            ):
+
+    use_dataset = False
+
+    # Generating folder and file path to save dataset of examples
+    folder_path = f"{rootpath}/ex_datasets/dataset={dataset.name}/archi={architecture.name}/"
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    file_path = ""
+    file_path += "adv_" if adv else "noisy_" if noise > 0 else "clean_"
+    file_path += "train_" if train else "test_"
+    file_path += str(attack_type)+"_" if adv else ""
+    file_path += "eps="+str(epsilon)+"_" if adv else ""
+    file_path += str(dataset_size)+"ex.pt"
+
+    # Check if a good dataset already exists
+    similar_file_path = "_".join(file_path.split("_")[:len(file_path.split("_"))-1])
+    use_file_name = None
+    for file in os.listdir(folder_path):
+        if similar_file_path in file:
+            use_file_name = file
+    if use_file_name:
+        nb_saved_ex = use_file_name.split("ex")[0].split("_")
+        nb_saved_ex = int(nb_saved_ex[len(nb_saved_ex)-1])
+        if dataset_size <= nb_saved_ex:
+            use_dataset = True
+            logger.info(f"We are going to use a saved dataset: {use_file_name} (dataset size required = {dataset_size})")
+            ex_dataset = torch.load(folder_path+use_file_name)
+        else:
+            ex_dataset = None
+    else:
+        ex_dataset = None
+    return ex_dataset, folder_path, file_path
+
+def get_point(
+    point_number: int,
+    saved_dataset: typing.Optional[typing.List],
+    dataset: Dataset,
+    adv: bool,
+    noise: float = 0,
+    epsilon: float = 0,
+    model: typing.Optional[torch.nn.Module] = None,
+    num_classes: int = 10,
+    attack_type: str = "FGSM",
+    num_iter: int = 10,
+    only_successful_adversaries: bool = True
+        ):
+    
+    point_rejected = True
+    nb_rejected = 0
+    if saved_dataset:
+        sample = [0,0]
+        sample[0], sample[1], x, y, y_pred = saved_dataset[point_number]
+    else:
+        while point_rejected:
+            sample = dataset.test_and_val_dataset[point_number]
+            x, y = process_sample(
+                sample=sample,
+                adversarial=adv,
+                noise=noise,
+                epsilon=epsilon,
+                model=model,
+                num_classes=10,
+                attack_type=attack_type,
+                num_iter=num_iter
+            )
+            y_pred = model(x).argmax(dim=-1).item()
+            if adv and only_successful_adversaries and y_pred == y:
+                logger.debug(f"Rejecting point (epsilon={epsilon}, y={y}, y_pred={y_pred}, adv={adv})")
+                nb_rejected += 1
+                point_number += 1
+                continue
+            else:
+                point_rejected = False
+                
+    return sample, x, y, y_pred, nb_rejected
 
 class DatasetLine(typing.NamedTuple):
     graph: Graph
@@ -132,6 +219,7 @@ def get_graph_dataset(
         num_iter: int = 10,
         start: int = 0,
         per_class: bool = False,
+        train: bool = True
 ) -> typing.Generator[DatasetLine, None, None]:
     # Else we have to compute the dataset first
     logger.info(f"Using source dataset {dataset.name}")
@@ -145,56 +233,83 @@ def get_graph_dataset(
     logger.info(f"Which attack ? {attack_type}")
 
     nb_samples = 0
-    i = start
     per_class_nb_samples = np.repeat(0, 10)
 
-    while nb_samples < dataset_size:
-        sample = dataset.test_and_val_dataset[i]
-        logger.debug(f"per class : {per_class_nb_samples} and nb samples = {nb_samples}")
-
-        x, y = process_sample(
-            sample=sample,
-            adversarial=adv,
-            noise=noise,
-            epsilon=epsilon,
-            model=architecture,
-            num_classes=10,
-            attack_type=attack_type,
-            num_iter=num_iter
+    ex_dataset, folder_path, file_path = load_saved_ex_dataset(
+        epsilon=epsilon,
+        noise=noise,
+        adv=adv,
+        dataset=dataset,
+        architecture=architecture,
+        dataset_size=dataset_size,
+        attack_type=attack_type,
+        train=train
         )
+    if ex_dataset:
+        i = 0
+        save_dataset = False
+    else:
+        i = start
+        save_dataset = True
+        list_examples = list()
+
+    while nb_samples < dataset_size:
+
+        sample, x, y, y_pred, nb_rejected = get_point(
+                point_number=i,
+                saved_dataset=ex_dataset,
+                dataset=dataset,
+                adv=adv,
+                noise=noise,
+                epsilon=epsilon,
+                model=architecture,
+                num_classes=10,
+                attack_type=attack_type,
+                num_iter=num_iter,
+                only_successful_adversaries=only_successful_adversaries
+                    )
+
+        i += nb_rejected
         if per_class and per_class_nb_samples[y] >= dataset_size:
             i += 1
             continue
+
         l2_norm = np.linalg.norm(torch.abs((sample[0].double() - x.double()).flatten()).detach().numpy(), 2)
         linf_norm = np.linalg.norm(torch.abs((sample[0].double() - x.double()).flatten()).detach().numpy(), np.inf)
-        y_pred = architecture(x).argmax(dim=-1).item()
-        y_adv = 0 if not adv else 1  # is it adversarial
+        
+        if save_dataset:
+            list_examples.append((sample[0], sample[1], x, y, y_pred))
 
-        if adv and only_successful_adversaries and y_pred == y:
-            logger.debug(
-                f"Rejecting point (epsilon={epsilon}, y={y}, y_pred={y_pred}, y_adv={y_adv}) and diff = {l2_norm}")
-            i += 1
-            continue
-        else:
-            x_graph = Graph.from_architecture_and_data_point(
+        edge_dict, layer_links = Graph.from_architecture_and_data_point_raw_dict(
                 architecture=architecture,
-                x=x.double(),
-                thresholds=thresholds
-            )
-            nb_samples += 1
-            i += 1
-            per_class_nb_samples[y] += 1
-            if per_class and any(np.asarray(per_class_nb_samples) < dataset_size):
-                nb_samples = 0
-            elif per_class and all(np.asarray(per_class_nb_samples) >= dataset_size):
-                nb_samples = dataset_size + 1
-            yield DatasetLine(
-                graph=x_graph,
-                y=y,
-                y_pred=y_pred,
-                y_adv=y_adv,
-                l2_norm=l2_norm,
-                linf_norm=linf_norm,
-                sample_id=i - 1,
-                x=x
-            )
+                x=x.double()
+                )
+
+        x_graph = Graph.from_architecture_and_data_point(
+            edge_dict=edge_dict,
+            layer_links=layer_links,
+            thresholds=thresholds
+        )
+
+        nb_samples += 1
+        if nb_samples % 10 == 0:
+            logger.info(f"computing sample number = {nb_samples}/{dataset_size}")
+        i += 1
+        per_class_nb_samples[y] += 1
+        if per_class and any(np.asarray(per_class_nb_samples) < dataset_size):
+            nb_samples = 0
+        elif per_class and all(np.asarray(per_class_nb_samples) >= dataset_size):
+            nb_samples = dataset_size + 1
+        if save_dataset and nb_samples >= dataset_size:
+            torch.save(list_examples, folder_path+file_path)
+            logger.info(f"Saved dataset examples in {file_path}")
+        yield DatasetLine(
+            graph=x_graph,
+            y=y,
+            y_pred=y_pred,
+            y_adv=adv,
+            l2_norm=l2_norm,
+            linf_norm=linf_norm,
+            sample_id=i - 1,
+            x=x
+        )
