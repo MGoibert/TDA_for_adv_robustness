e4fd4beaeb06caf037fbd3ed08176c4af47bd480
Thomas Ricatte <thomas@ricatte.fr>
2020-01-29 19:02:28 +0100

    Speeding up gram matrices (joblib)

--- tda/embeddings/__init__.py
+++ tda/embeddings/__init__.py
@@ -7,6 +7,7 @@ from tda.embeddings.persistent_diagrams import sliced_wasserstein_kernel, \
     compute_dgm_from_graph
 from tda.graph_dataset import DatasetLine
 from tda.models import Architecture
+from joblib import Parallel, delayed
 
 
 class EmbeddingType(object):
@@ -55,7 +56,7 @@ def get_embedding(
         return sorted(graph.final_logits)
 
 
-def get_gram_matrix(
+def get_gram_matrix_legacy(
         kernel_type: str,
         embeddings_in: List,
         embeddings_out: Optional[List] = None,
@@ -112,4 +113,70 @@ def get_gram_matrix(
                 )
             if sym:
                 gram[j, i] = gram[i, j]
+    return gram
+
+
+def get_gram_matrix(
+        kernel_type: str,
+        embeddings_in: List,
+        embeddings_out: Optional[List] = None,
+        params: Dict = dict()
+):
+    """
+    Compute the gram matrix of the given embeddings
+    using a specific kernel
+
+    :param kernel_type: Kernel to be used (str)
+    :param embeddings_in: Embeddings in
+    :param embeddings_out: Embeddings out
+        if None, we use the same as embeddings_in and use the fact
+        the matrix is symmetric to speed up the process
+    :param params: Parameters for the kernel
+    :return:
+    """
+
+    # If no embeddings out is specified, we use embeddings_in
+    embeddings_out = embeddings_out or embeddings_in
+
+    n = len(embeddings_in)
+    m = len(embeddings_out)
+
+    def compute_gram_chunk(my_slices):
+        ret = list()
+        for i, j in my_slices:
+            if kernel_type == KernelType.Euclidean:
+                ret.append(np.transpose(embeddings_in[i]) @ embeddings_out[j])
+            elif kernel_type == KernelType.RBF:
+                ret.append(np.exp(-np.linalg.norm(
+                    embeddings_in[i] - embeddings_out[j]
+                ) / 2 * params['gamma'] ** 2))
+            elif kernel_type == KernelType.SlicedWasserstein:
+                sw = sliced_wasserstein_kernel(
+                    embeddings_in[i],
+                    embeddings_out[j],
+                    M=params['M'])
+                ret.append(np.exp(-sw / (2 * params['sigma'] ** 2)))
+            else:
+                raise NotImplementedError(
+                    f"Unknown kernel {kernel_type}"
+                )
+        return ret
+
+    nb_jobs = 8
+
+    p = Parallel(n_jobs=nb_jobs)
+
+    all_indices = [(i, j) for j in range(m) for i in range(n)]
+
+    def chunks(lst, n):
+        """Yield successive n-sized chunks from lst."""
+        for i in range(0, len(lst), n):
+            yield lst[i:i + n]
+
+    my_chunks = chunks(all_indices, len(all_indices) // nb_jobs)
+
+    gram = p([delayed(compute_gram_chunk)(chunk) for chunk in my_chunks])
+    gram = [item for sublist in gram for item in sublist]
+    gram = np.reshape(gram, (n, m))
+
     return gram
--- test/test_persistent_diagrams.py
+++ test/test_persistent_diagrams.py
@@ -1,18 +1,12 @@
 import numpy as np
 import torch
 
-from tda.embeddings.persistent_diagrams import compute_dgm_from_graph, \
-    compute_dgm_from_graph, sliced_wasserstein_kernel
+from tda.embeddings.persistent_diagrams import compute_dgm_from_graph, sliced_wasserstein_kernel
 from tda.graph import Graph
 from tda.models import Architecture
 from tda.models.architectures import LinearLayer, SoftMaxLayer
 
-from ripser import Rips
-from persim.sliced_wasserstein import sliced_wasserstein
-
-
-def test_ripser_vs_dionysus():
-    simple_archi: Architecture = Architecture(
+simple_archi: Architecture = Architecture(
         preprocess=lambda x: x,
         layers=[
             LinearLayer(4, 3),
@@ -21,32 +15,26 @@ def test_ripser_vs_dionysus():
             SoftMaxLayer()
         ])
 
-    ex1 = torch.ones(4) * 42
-    ex2 = torch.ones(4) * 37
+ex1 = torch.ones(4) * 42
+ex2 = torch.ones(4) * 37
 
-    g1 = Graph.from_architecture_and_data_point(simple_archi, ex1, thresholds=dict())
-    g2 = Graph.from_architecture_and_data_point(simple_archi, ex2, thresholds=dict())
+g1 = Graph.from_architecture_and_data_point(simple_archi, ex1, thresholds=dict())
+g2 = Graph.from_architecture_and_data_point(simple_archi, ex2, thresholds=dict())
 
-    ############
-    # Dionysus #
-    ############
+############
+# Dionysus #
+############
 
-    dgm1 = compute_dgm_from_graph(g1)
-    dgm2 = compute_dgm_from_graph(g2)
+dgm1 = compute_dgm_from_graph(g1)
+dgm2 = compute_dgm_from_graph(g2)
 
-    k_dionysus = sliced_wasserstein_kernel(dgm1, dgm2)
 
-    for pt in dgm1:
-        print(pt)
-    print(k_dionysus)
+def test_sliced_wassertstein_kernel(benchmark):
 
-    ##########
-    # Ripser #
-    ##########
+    def compute_dgm():
+        return sliced_wasserstein_kernel(dgm1, dgm2)
 
-    ret = Rips(maxdim=2, coeff=1)
-    #print(g1.get_adjacency_matrix())
-    z = ret.fit_transform(g1.get_adjacency_matrix(), distance_matrix=True)
+    k_dionysus = benchmark(compute_dgm)
+    assert np.isclose(k_dionysus, 7.450580596923829e-10, rtol=1e-12)
 
-    Rips().plot(z)
-    print(z)
+
