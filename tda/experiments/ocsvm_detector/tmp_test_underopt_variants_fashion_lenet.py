92a1a5396a4370e5674f9e7d634ac4eaa8d9a373
Thomas Ricatte <thomas@ricatte.fr>
2020-03-04 11:30:23 +0100

    Adding variant experiment for underopt strats

---
+++ tda/experiments/ocsvm_detector/tmp_test_underopt_variants_fashion_lenet.py
@@ -0,0 +1,48 @@
+from r3d3 import R3D3Experiment
+
+from tda.embeddings import EmbeddingType, KernelType, ThresholdStrategy
+from tda.models.architectures import fashion_mnist_lenet
+from tda.rootpath import rootpath, db_path
+
+experiment = R3D3Experiment(
+    db_path=db_path,
+    configs={
+        'embedding_type': [
+            EmbeddingType.PersistentDiagram
+        ],
+        'kernel_type': [
+            KernelType.SlicedWasserstein
+        ],
+        'architecture': [
+            fashion_mnist_lenet.name
+        ],
+        'threshold_strategy': [
+            ThresholdStrategy.ActivationValue,
+            ThresholdStrategy.UnderoptimizedMagnitudeIncrease,
+            ThresholdStrategy.UnderoptimizedMagnitudeIncreaseV2
+        ],
+        'epochs': [
+            200
+        ],
+        'dataset': [
+            "FashionMNIST"
+        ],
+        'dataset_size': [
+            100
+        ],
+        'attack_type': [
+            "FGSM", "DeepFool"
+        ],
+        'thresholds': [
+            '0.1_0.0_0.0_0.0_0.0_0.0'
+        ],
+        'noise': [
+            0.0
+        ],
+        'n_jobs': [
+            24
+        ]
+    },
+    binary=f"{rootpath}/tda/experiments/ocsvm_detector/ocsvm_detector_binary.py",
+    max_nb_processes=1
+)
