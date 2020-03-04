d7ff12f4c3b2f7b619b21c2aa5703d813402a3bb
Thomas Ricatte <thomas@ricatte.fr>
2020-03-04 12:00:49 +0100

    Improving model definition
    * Splitting in different files for better
    * Adding FashionMNIST model with 0/1 pixels

--- tda/models/architectures/__init__.py
+++ tda/models/architectures/__init__.py
@@ -1,20 +1,26 @@
 from typing import List
 from .architecture import Architecture
-from .definitions import (
+
+from .mnist_models import (
     mnist_lenet,
     mnist_mlp,
-    svhn_lenet,
-    svhn_cnn_simple,
-    svhn_resnet,
-    cifar_lenet,
-    fashion_mnist_lenet,
-    svhn_preprocess,
-    mnist_preprocess,
-    mnist_preprocess2,
-    fashion_mnist_mlp,
+    mnist_preprocess_flatten,
+    mnist_preprocess_cnn,
+    mnist_preprocess_cnn_0_1,
     mnist_small_mlp,
-    svhn_resnet_test,
 )
+
+from .svhn_models import (
+    svhn_lenet,
+    svhn_resnet,
+    svhn_resnet_test,
+    svhn_cnn_simple,
+    svhn_preprocess,
+)
+
+from .fashion_mnist_models import fashion_mnist_mlp, fashion_mnist_lenet
+
+from .cifar10_models import cifar_lenet
 
 known_architectures: List[Architecture] = [
     mnist_mlp,
