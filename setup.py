ef3e6c9d1680627d858e1799c4b3cacc463249cd
Thomas Ricatte <thomas@ricatte.fr>
2020-03-10 16:31:05 +0100

    Removing tda_env from git

--- setup.py
+++ setup.py
@@ -7,17 +7,18 @@ import os
 def _read_reqs(relpath):
     fullpath = os.path.join(os.path.dirname(__file__), relpath)
     with open(fullpath) as f:
-        return [s.strip() for s in f.readlines()
-                if (s.strip() and not s.startswith("#"))]
+        return [
+            s.strip() for s in f.readlines() if (s.strip() and not s.startswith("#"))
+        ]
 
 
 _REQUIREMENTS_TXT = _read_reqs("requirements.txt")
 _INSTALL_REQUIRES = [l for l in _REQUIREMENTS_TXT if "://" not in l]
 
 setuptools.setup(
-    name='tda',
-    version='0.2',
+    name="tda",
+    version="0.2",
     install_requires=_INSTALL_REQUIRES,
-    data_files=[('.', ['requirements.txt'])],
+    data_files=[(".", ["requirements.txt"])],
     packages=setuptools.find_packages(),
 )
