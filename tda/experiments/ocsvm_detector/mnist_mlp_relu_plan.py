44ae9ce32b82e95ff0dddf64995ec3ecc43b04a0
Thomas Ricatte <thomas@ricatte.fr>
2020-04-03 14:41:47 +0200

    Updating experiment plan to include lid

--- tda/experiments/ocsvm_detector/mnist_mlp_relu_plan.py
+++ tda/experiments/ocsvm_detector/mnist_mlp_relu_plan.py
@@ -2,10 +2,7 @@ from r3d3.experiment import R3D3ExperimentPlan, R3D3Experiment
 from r3d3.utils import cartesian_product
 
 from tda.embeddings import EmbeddingType, KernelType, ThresholdStrategy
-from tda.models.architectures import (
-    mnist_mlp,
-    mnist_mlp_relu
-)
+from tda.models.architectures import mnist_mlp, mnist_mlp_relu
 from tda.rootpath import rootpath, db_path
 from copy import deepcopy
 
@@ -20,7 +17,7 @@ base_configs = cartesian_product(
         "n_jobs": [8],
         "all_epsilons": ["0.01;0.1;0.4"],
         "dataset": ["MNIST"],
-        "epochs": [50]
+        "epochs": [50],
     }
 )
 
@@ -29,11 +26,7 @@ binary = f"{rootpath}/tda/experiments/ocsvm_detector/ocsvm_detector_binary.py"
 all_experiments = list()
 
 for threshold, threshold_strategy, sigmoidize in [
-    [
-        "0:0.3_1:0.3_2:0.0",
-        ThresholdStrategy.UnderoptimizedMagnitudeIncrease,
-        True
-    ],
+    ["0:0.3_1:0.3_2:0.0", ThresholdStrategy.UnderoptimizedMagnitudeIncrease, True],
 ]:
     for config in base_configs:
         config = deepcopy(config)
@@ -43,6 +36,48 @@ for threshold, threshold_strategy, sigmoidize in [
 
         all_experiments.append(R3D3Experiment(binary=binary, config=config))
 
-experiment_plan = R3D3ExperimentPlan(
+        config_lid = deepcopy(config)
+
+experiment_plan_ours = R3D3ExperimentPlan(
     experiments=all_experiments, max_nb_processes=1, db_path=db_path
+)
+
+#######
+# LID #
+#######
+
+base_configs = cartesian_product(
+    {
+        "attack_type": ["FGSM"],
+        "noise": [0.0],
+        "dataset_size": [500],
+        "batch_size": [100],
+        "perc_of_nn": [0.2],
+        "successful_adv": [1],
+        "train_noise": [0.0],
+    }
+)
+
+binary = f"{rootpath}/tda/experiments/lid/lid_binary.py"
+
+all_experiments = list()
+
+for model, dataset, nb_epochs in [
+    [mnist_mlp.name, "MNIST", 50],
+    [mnist_mlp_relu.name, "MNIST", 50],
+]:
+    for config in base_configs:
+        config = deepcopy(config)
+        config["architecture"] = model
+        config["dataset"] = dataset
+        config["epochs"] = nb_epochs
+
+        all_experiments.append(R3D3Experiment(binary=binary, config=config))
+
+experiment_plan_lid = R3D3ExperimentPlan(
+    experiments=all_experiments, max_nb_processes=4, db_path=db_path
+)
+
+experiment_plan = R3D3ExperimentPlan.from_multiple_plans(
+    [experiment_plan_lid, experiment_plan_ours]
 )
