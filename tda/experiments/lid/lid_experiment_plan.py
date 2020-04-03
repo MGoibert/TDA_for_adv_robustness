91fbd12f934a6e50e4f93eb42aa473cac052d07a
Morgane Goibert <morgane.goibert@gmail.com>
2020-04-03 09:44:06 +0200

    garbage commit

--- tda/experiments/lid/lid_experiment_plan.py
+++ tda/experiments/lid/lid_experiment_plan.py
@@ -6,6 +6,8 @@ from tda.models.architectures import (
     fashion_mnist_lenet,
     fashion_mnist_mlp,
     svhn_lenet,
+    svhn_lenet_bandw,
+    svhn_lenet_bandw2,
     cifar_lenet,
 )
 from tda.rootpath import rootpath, db_path
@@ -13,13 +15,14 @@ from copy import deepcopy
 
 base_configs = cartesian_product(
     {
-        "attack_type": ["FGSM", "DeepFool"],
-        "noise": [0.01],
-        "dataset_size": [500],
+        "attack_type": ["FGSM"],
+        "noise": [0.0],
+        "dataset_size": [100],
         "batch_size": [100],
         "perc_of_nn": [0.2],
         "successful_adv": [1],
         "train_noise": [0.0],
+        "all_epsilons": ["0.01;0.1;0.4"]
     }
 )
 
@@ -28,12 +31,13 @@ binary = f"{rootpath}/tda/experiments/lid/lid_binary.py"
 all_experiments = list()
 
 for model, dataset, nb_epochs in [
-    [mnist_mlp.name, "MNIST", 50],
-    [mnist_lenet.name, "MNIST", 50],
-    [fashion_mnist_mlp.name, "FashionMNIST", 50],
-    [fashion_mnist_lenet.name, "FashionMNIST", 200],
-    [svhn_lenet.name, "SVHN", 250],
-    [cifar_lenet.name, "CIFAR10", 300],
+    #[mnist_mlp.name, "MNIST", 50],
+    #[mnist_lenet.name, "MNIST", 50],
+    #[fashion_mnist_mlp.name, "FashionMNIST", 50],
+    #[fashion_mnist_lenet.name, "FashionMNIST", 200],
+    #[svhn_lenet.name, "SVHN", 250],
+    [svhn_lenet_bandw2.name, "SVHN_BandW", 200],
+    #[cifar_lenet.name, "CIFAR10", 300],
 ]:
     for config in base_configs:
         config = deepcopy(config)
