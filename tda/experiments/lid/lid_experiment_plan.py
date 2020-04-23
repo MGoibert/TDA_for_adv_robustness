3a6b53eacac6b5300a1c10611496b5d3b442c737
Thomas Ricatte <thomas@ricatte.fr>
2020-04-23 11:15:33 +0200

    Updating LID

--- tda/experiments/lid/lid_experiment_plan.py
+++ tda/experiments/lid/lid_experiment_plan.py
@@ -18,8 +18,6 @@ base_configs = cartesian_product(
         "attack_type": ["FGSM"],
         "noise": [0.0],
         "dataset_size": [500],
-        "batch_size": [100],
-        "perc_of_nn": [0.2],
         "successful_adv": [1],
         "train_noise": [0.0],
         "all_epsilons": ["0.01;0.1;0.4"],
@@ -30,20 +28,22 @@ binary = f"{rootpath}/tda/experiments/lid/lid_binary.py"
 
 all_experiments = list()
 
-for model, dataset, nb_epochs in [
-    [mnist_mlp.name, "MNIST", 50],
-    [mnist_lenet.name, "MNIST", 50],
-    [fashion_mnist_mlp.name, "FashionMNIST", 50],
-    [fashion_mnist_lenet.name, "FashionMNIST", 200],
-    [svhn_lenet.name, "SVHN", 250],
-    [svhn_lenet_bandw2.name, "SVHN_BandW", 200],
-    [cifar_lenet.name, "CIFAR10", 300],
+for model, dataset, nb_epochs, perc_of_nn, batch_size in [
+    [mnist_mlp.name, "MNIST", 50, 0.1, 250],  # Not benched
+    [mnist_lenet.name, "MNIST", 50, 0.08, 250],
+    [fashion_mnist_mlp.name, "FashionMNIST", 50, 0.1, 250],  # Not benched
+    [fashion_mnist_lenet.name, "FashionMNIST", 200, 0.02, 250],
+    [svhn_lenet.name, "SVHN", 250, 0.1, 250],
+    [svhn_lenet_bandw2.name, "SVHN_BandW", 200, 0.1, 250],  # Not benched
+    [cifar_lenet.name, "CIFAR10", 300, 0.3, 100],
 ]:
     for config in base_configs:
         config = deepcopy(config)
         config["architecture"] = model
         config["dataset"] = dataset
         config["epochs"] = nb_epochs
+        config["perc_of_nn"] = perc_of_nn
+        config["batch_size"] = batch_size
 
         all_experiments.append(R3D3Experiment(binary=binary, config=config))
 
