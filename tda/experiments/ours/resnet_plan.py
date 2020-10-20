5bfc49c53d0aa53d97d0934aabd9ca8aef21cfc4
m.goibert <m.goibert@mesos-slave109-pa4.central.criteo.prod>
2020-10-20 12:48:13 +0000

    Official threshold and exp for cifar resnet

--- tda/experiments/ours/resnet_plan.py
+++ tda/experiments/ours/resnet_plan.py
@@ -12,11 +12,11 @@ base_configs = cartesian_product(
     {
         "embedding_type": [EmbeddingType.PersistentDiagram],
         "dataset_size": [500],
-        "attack_type": [AttackType.PGD, AttackType.CW],
+        "attack_type": [AttackType.FGSM, AttackType.PGD, AttackType.DeepFool, AttackType.CW, AttackType.BOUNDARY],
         "attack_backend": [AttackBackend.FOOLBOX],
         "noise": [0.0],
-        "n_jobs": [8],
-        "all_epsilons": ["0.01;0.1;0.4"],
+        "n_jobs": [1],
+        "all_epsilons": ["0.01;0.05;0.1"],
         "raw_graph_pca": [-1],
     }
 )
@@ -33,30 +33,15 @@ for (
     threshold_strategy,
     sigmoidize_rawgraph,
 ) in [
-    # [
-    #    cifar_toy_resnet.name,
-    #    "CIFAR10",
-    #    300,
-    #    "0.05",
-    #    ThresholdStrategy.UnderoptimizedMagnitudeIncrease,
-    #    True
-    # ],
     [
         cifar_resnet_1.name,
         "CIFAR10",
         100,
-        "1:0.05_34:0.05_35:0.05_0:0.05",
-        ThresholdStrategy.UnderoptimizedMagnitudeIncrease,
+        '39:0.075_40:0.075_41:0.075_42:0.075_43:0.075',
+        ThresholdStrategy.UnderoptimizedMagnitudeIncreaseV3,
         True,
     ],
-    [
-        cifar_resnet_1.name,
-        "CIFAR10",
-        100,
-        "1:0.05_16:0.05_17:0.05_34:0.05_35:0.05_0:0.05",
-        ThresholdStrategy.UnderoptimizedMagnitudeIncrease,
-        True,
-    ],
+       
 ]:
     for config in base_configs:
         config = deepcopy(config)
@@ -67,8 +52,8 @@ for (
         config["threshold_strategy"] = threshold_strategy
         config["sigmoidize"] = sigmoidize_rawgraph
 
-        if config["embedding_type"] == EmbeddingType.PersistentDiagram:
-            config["sigmoidize"] = True
+        #if config["embedding_type"] == EmbeddingType.PersistentDiagram:
+        #    config["sigmoidize"] = True
 
         if config["attack_type"] not in ["FGSM", "PGD"]:
             config["all_epsilons"] = "1.0"
@@ -76,5 +61,5 @@ for (
         all_experiments.append(R3D3Experiment(binary=binary, config=config))
 
 experiment_plan = R3D3ExperimentPlan(
-    experiments=all_experiments, max_nb_processes=1, db_path=db_path
+      experiments=all_experiments, max_nb_processes=1, db_path=db_path
 )
