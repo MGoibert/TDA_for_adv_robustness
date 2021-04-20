c5302c286affdaf1fe89786d8d2bccaa37192044
Thomas Ricatte <thomas@ricatte.fr>
2021-04-20 12:20:10 +0200

    Fixing conv layer

--- test/models/layers/test_build_matrix.py
+++ test/models/layers/test_build_matrix.py
@@ -1,21 +1,84 @@
 import torch
 import pytest
 import numpy as np
-from tda.models.layers import LinearLayer, ConvLayer, AvgPool2dLayer, AdaptativeAvgPool2dLayer
+from tda.models.layers import (
+    LinearLayer,
+    ConvLayer,
+    AvgPool2dLayer,
+    AdaptativeAvgPool2dLayer,
+)
 
 
-@pytest.mark.parametrize("layer,input_shape", [
-    (LinearLayer(3, 2, activ=None, bias=False), (3, 1)),
-    (ConvLayer(in_channels=2, out_channels=3, kernel_size=2, input_shape=(7, 7)), (1, 2, 7, 7)),
-    (ConvLayer(in_channels=2, out_channels=3, kernel_size=2, stride=2, input_shape=(7, 7)), (1, 2, 7, 7)),
-    (ConvLayer(in_channels=2, out_channels=2, kernel_size=2, grouped_channels=True, input_shape=(7, 7)), (1, 2, 7, 7)),
-    (AvgPool2dLayer(kernel_size=3, ceil_mode=False), (1, 2, 7, 7)),
-    (AvgPool2dLayer(kernel_size=3, ceil_mode=True), (1, 2, 7, 7)),
-    (AdaptativeAvgPool2dLayer(output_size=(2, 2)), (1, 2, 7, 7))
-])
+@pytest.mark.parametrize(
+    "layer,input_shape",
+    [
+        (LinearLayer(3, 2, activ=None, bias=False), (3, 1)),
+        (
+            ConvLayer(in_channels=2, out_channels=3, kernel_size=2, input_shape=(7, 7)),
+            (1, 2, 7, 7),
+        ),
+        (
+            ConvLayer(
+                in_channels=1,
+                out_channels=1,
+                kernel_size=2,
+                stride=2,
+                input_shape=(4, 4),
+            ),
+            (1, 1, 4, 4),
+        ),
+        (
+            ConvLayer(
+                in_channels=2,
+                out_channels=2,
+                kernel_size=2,
+                grouped_channels=True,
+                input_shape=(7, 7),
+            ),
+            (1, 2, 7, 7),
+        ),
+        (
+            ConvLayer(
+                in_channels=1,
+                out_channels=1,
+                kernel_size=(2, 1),
+                stride=2,
+                padding=0,
+                input_shape=(5, 5),
+            ),
+            (1, 1, 5, 5),
+        ),
+        (
+            ConvLayer(
+                in_channels=1,
+                out_channels=1,
+                kernel_size=(2, 1),
+                stride=2,
+                padding=1,
+                input_shape=(5, 5),
+            ),
+            (1, 1, 5, 5),
+        ),
+        (
+            ConvLayer(
+                in_channels=2,
+                out_channels=3,
+                kernel_size=(3, 2),
+                stride=2,
+                padding=2,
+                input_shape=(11, 13),
+            ),
+            (1, 2, 11, 13),
+        ),
+        (AvgPool2dLayer(kernel_size=3, ceil_mode=False), (1, 2, 7, 7)),
+        (AvgPool2dLayer(kernel_size=3, ceil_mode=True), (1, 2, 7, 7)),
+        (AdaptativeAvgPool2dLayer(output_size=(2, 2)), (1, 2, 7, 7)),
+    ],
+)
 def test_build_matrix(layer, input_shape):
+    sample = torch.randn(input_shape)
 
-    input = {0: torch.randn(input_shape)}
+    input = {0: sample}
 
     out = layer.process(input, store_for_graph=True)
     print(f"Out shape is {out.shape}")
